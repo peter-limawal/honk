38abfa8a9908799b4805a11c333131cdd80f5d96
Peter Limawal <peterlimawal007@gmail.com>
2023-03-16 00:55:38 -0400

    implement basic timer

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,44 @@
-// TODO: background script
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: on installed function
+// background.ts
+let timerActive = false
+let remainingTime = 25 * 60
+let initialTime = 25 * 60
+
+chrome.runtime.onConnect.addListener((port) => {
+  if (port.name === 'popup') {
+    port.onMessage.addListener((message) => {
+      if (message.type === 'getTimerState') {
+        port.postMessage({
+          type: 'updateTimerState',
+          timerActive,
+          remainingTime,
+        })
+      }
+    })
+  }
+})
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'getTimerState') {
+    sendResponse({ timerActive, remainingTime })
+  } else if (message.type === 'toggleTimer') {
+    console.log('toggleTimer called')
+    timerActive = !timerActive
+
+    if (timerActive) {
+      chrome.alarms.create('timer', { periodInMinutes: 1 / 60 })
+    } else {
+      chrome.alarms.clear('timer')
+    }
+  }
+})
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'timer') {
+    remainingTime -= 1
+    if (remainingTime <= 0) {
+      timerActive = false
+      remainingTime = initialTime
+      chrome.alarms.clear('timer')
+    }
+  }
 })
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,82 @@
-import React from 'react'
+// popup.tsx
+import React, { useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './popup.css'
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 const App: React.FC<{}> = () => {
+  const initialTime = 25 * 60 // 25 minutes in seconds
+  const [timerActive, setTimerActive] = useState(false)
+  const [remainingTime, setRemainingTime] = useState(initialTime)
+
+  useEffect(() => {
+    const connectedPort = chrome.runtime.connect({ name: 'popup' })
+
+    connectedPort.postMessage({ type: 'getTimerState' })
+
+    connectedPort.onMessage.addListener((response) => {
+      if (response.type === 'updateTimerState') {
+        setTimerActive(response.timerActive)
+        setRemainingTime(response.remainingTime)
+      }
+    })
+
+    // Add this interval to request updates every second
+    const updateInterval = setInterval(() => {
+      connectedPort.postMessage({ type: 'getTimerState' })
+    }, 1000)
+
+    return () => {
+      connectedPort.disconnect()
+      clearInterval(updateInterval)
+    }
+  }, [])
+
+  const toggleTimer = () => {
+    console.log('toggleTimer called')
+    chrome.runtime.sendMessage({ type: 'toggleTimer' })
+  }
+
+  const formatTime = (timeInSeconds: number) => {
+    const minutes = Math.floor(timeInSeconds / 60)
+    const seconds = timeInSeconds % 60
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`
+  }
+
   return (
-    <div>
-      <img src="icon.png" />
+    <div className="container">
+      <h1 className="title">Honk!</h1>
+      <p className="subtitle">Pomodoro Study Timer</p>
+      <div className="circular-progressbar-container">
+        <CircularProgressbarWithChildren
+          value={((initialTime - remainingTime) / initialTime) * 100}
+          styles={buildStyles({
+            strokeLinecap: 'butt',
+            pathColor: 'turquoise',
+            trailColor: '#eee',
+            textSize: '16px',
+          })}
+        >
+          <img
+            style={{ width: '50%', height: 'auto' }}
+            src={chrome.runtime.getURL('assets/goose1.png')}
+            alt="mr goose"
+          />
+        </CircularProgressbarWithChildren>
+      </div>
+      <p className="timer">{formatTime(remainingTime)}</p>
+      <button
+        className={`timer-toggle ${timerActive ? 'active' : ''}`}
+        onClick={toggleTimer}
+      >
+        {timerActive ? 'Stop Studying' : 'Start Studying'}
+      </button>
     </div>
   )
 }
